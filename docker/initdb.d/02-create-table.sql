b01b85f5d681d3bb1d0f6c3e4415529d7f6c134f
tae0y <tae0yp@gmail.com>
2025-02-02 21:04:56 +0900

    staging working codes

--- docker/initdb.d/02-create-table.sql
+++ docker/initdb.d/02-create-table.sql
@@ -1,33 +1,30 @@
 BEGIN;
 
 CREATE TABLE tb_concepts (
-            id serial primary key,
-            title text,
-            datasource text,
-            keywords text,
-            category text,
-            summary text,
-            status text,
-            filepath text,
-            source_num integer,
-            target_num integer,
-            create_time timestamp,
-            update_time timestamp,
-            token_num integer,
-            plaintext text,
-            embedding vector(4096)
+    id                  serial  primary key,
+    title               text,
+    keywords            text,
+    category            text,
+    summary             text,
+    status              text,
+    data_name           text,
+    source_num          integer,
+    target_num          integer,
+    create_time         timestamp,
+    update_time         timestamp,
+    embedding           vector(4096)
 );
 
 CREATE TABLE tb_networks (
-            id serial primary key,
-            source_concept_id integer,
-            target_concept_id integer
+    id                  serial primary key,
+    source_concept_id   integer,
+    target_concept_id   integer
 );
 
 CREATE TABLE tb_references (
-            id serial primary key,
-            concept_id integer,
-            description text
+    id                  serial primary key,
+    concept_id          integer,
+    description         text
 );
 
 END;
