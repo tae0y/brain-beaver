d7e6d5890e096b04dddb49d7169de0be62b83837
tae0y <tae0yp@gmail.com>
2025-02-08 15:19:19 +0900

    update : logging 추가

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -4,14 +4,16 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var portValueStr = Environment.GetEnvironmentVariable("UVICORN_PORT");
 var portValueInt = int.TryParse(portValueStr, out int portParsed) ? portParsed : 8111;
-Console.WriteLine($"PORT: {portValueInt}");
 
 // Python Hosting은 실험적 기능으로, 추가 방법이 바뀔 수 있어 Warning을 띄움. 테스트 용도로만 사용이 권장됨.
 #pragma warning disable ASPIREHOSTINGPYTHON001
-var uvicorn = builder.AddUvicornApp(
+var uvicornApp = builder.AddUvicornApp(
         name: "python",                // Name of the Python project
         projectDirectory: "../python", // Path to the Python project
-        appName: "app:app"             // {FILE_NAME}:{APP_VARIABLE_NAME}
+        appName: "app:app",            // {FILE_NAME}:{APP_VARIABLE_NAME}
+        args: new[] { 
+            "--reload"
+        }
     )
     // 무조건 환경변수로 지정된 PORT를 따라가는 이슈가 있어 export PORT=8000 같이 지정해줌
     .WithHttpEndpoint(
--- python/app.py
+++ python/app.py
@@ -1,8 +1,27 @@
 import os
+import logging
+import uvicorn
 from fastapi import FastAPI
+#from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
 
+#FastAPIInstrumentor().instrument_app(app)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.get("/")
 def rootPage():
+    logger.info("rootPage accessed")
     return "Hello World"
+
+if __name__ == "__main__":
+    """
+    디버깅을 위해 역으로 파이썬 안에서 Uvicorn을 호출
+    """
+    uvicorn.run(
+        app=app,
+        host="0.0.0.0",
+        port=int(os.environ.get("UVICORN_PORT", 8111))
+    )
