bde9661d727a1607599c9e0f9e307206740d05b9
tae0y <tae0yp@gmail.com>
2025-02-08 14:09:51 +0900

    add : Aspire UvicornHosting 커뮤니티 패키지 추가

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -1,3 +1,23 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var portValueStr = Environment.GetEnvironmentVariable("UVICORN_PORT");
+var portValueInt = int.TryParse(portValueStr, out int portParsed) ? portParsed : 8111;
+Console.WriteLine($"PORT: {portValueInt}");
+
+// Python Hosting은 실험적 기능으로, 추가 방법이 바뀔 수 있어 Warning을 띄움. 테스트 용도로만 사용이 권장됨.
+#pragma warning disable ASPIREHOSTINGPYTHON001
+var uvicorn = builder.AddUvicornApp(
+        name: "python",                // Name of the Python project
+        projectDirectory: "../python", // Path to the Python project
+        appName: "app:app"             // {FILE_NAME}:{APP_VARIABLE_NAME}
+    )
+    // 무조건 환경변수로 지정된 PORT를 따라가는 이슈가 있어 export PORT=8000 같이 지정해줌
+    .WithHttpEndpoint(
+        targetPort: portValueInt,      // tatgetPort : Port the resource is listening on
+        port: portValueInt + 1         // port : Port that will be exposed to the outside
+    );
+#pragma warning restore ASPIREHOSTINGPYTHON001
 
 builder.Build().Run();
--- python/app.py
+++ python/app.py
@@ -1,52 +1,8 @@
 import os
-from extract.concepts.conceptshandler import ConceptsHandler
-from engage.networks.networkshandler import NetworksHandler
-from expand.references.referenceshandler import ReferencesHandler
+from fastapi import FastAPI
 
-def main():
-    conceptHandler = ConceptsHandler()
-    networkHandler = NetworksHandler()
-    referenceHandler = ReferencesHandler()
+app = FastAPI()
 
-    concepts_count_before = conceptHandler.get_concepts_all_count()
-    #TODO: 처리내역이 있을 경우 이어서 진행하는 로직 추가
-    #if concepts_count_before == 0:
-
-    # 지식추출
-    conceptHandler.extract_keyconcepts_from_datasource(
-        datasourcetype='markdown',
-        datasourcepath='/Users/bachtaeyeong/20_DocHub/TIL',
-        options={
-            'reason_model_name' : 'gemma2:9b-instruct-q5_K_M',
-            'embed_model_name' : 'gemma2:9b-instruct-q5_K_M',
-            #'reason_model_name' : 'gpt-4o-mini',
-            #'embed_model_name' : 'text-embedding-3-small',
-            'max_file_num' : 2,
-            'max_budget_won' : 1000
-        }
-    )
-
-    # 지식연결
-    networkHandler.engage_keyconcepts_into_networks(
-        options={'operation' : 'cosine_distance'}
-    )
-
-    conceptHandler.update_concepts_source_target_count()
-
-    # 지식확장
-    concepts_count_after = conceptHandler.get_concepts_all_count()
-    referenceHandler.reset_expand_keyconcpts()
-    referenceHandler.expand_keyconcepts_with_websearch(
-        options = {
-            'reason_model_name' : 'gemma2:9b-instruct-q5_K_M',
-            #'reason_model_name' : 'gpt-4o-mini',
-            'action_type' : 'top',
-            'action_limit' : concepts_count_after//2
-        }
-    )
-
-    os.system('streamlit run view.py')
-    pass
-
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def rootPage():
+    return "Hello World"
