5973a7d97818aa4fce9fd48f51389a9bd3c1ccaa
tae0y <tae0yp@gmail.com>
2025-02-12 20:06:30 +0900

    update : aspire 컨테이너간 의존관계 추가

--- src/Aspire.AppHost/Program.cs
+++ src/Aspire.AppHost/Program.cs
@@ -11,45 +11,42 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // AppHost의 DockerContainer는 상대경로 기반 Volume을 제공하지 않음
 // 디렉토리명만 제공시 OrbStack/Docker/Volume 아래 생성됨
-var apsolutepath_initdbd_volume = Path.GetFullPath("../../docker/initdb.d");
-//var apsolutepath_portainer_volume = Path.GetFullPath("../../docker/portainer_volume");
-//var apsolutepath_postgresql_volume = Path.GetFullPath("../../docker/postgresql_volume");
-//var apsolutepath_pgadmin_volume = Path.GetFullPath("../../docker/pgadmin_volume");
-var justdirectory_portainer_volume = "portainer_volume";
+var apsolutepath_initdbd_volume     = Path.GetFullPath("../../docker/initdb.d");
+var justdirectory_portainer_volume  = "portainer_volume";
 var justdirectory_postgresql_volume = "postgresql_volume";
-var justdirectory_pgadmin_volume = "pgadmin_volume";
+var justdirectory_pgadmin_volume    = "pgadmin_volume";
 
 var portainerContainer = builder.AddDockerfile(
-        "dockeradmin",   
+        "dockeradmin",
         "../../docker",          // 도커파일이 위치한 디렉토리 상대경로 (AppHost.csproj 기준)
-        "Dockerfile-dockeradmin"    // 도커파일 이름
+        "Dockerfile-dockeradmin" // 도커파일 이름
     )
-    .WithContainerName("dockeradmin")
-    .WithHttpEndpoint(9000, 9000)
+    .WithContainerName("bwscontaineradmin")
+    .WithHttpEndpoint(port:9000, targetPort:9000)
     .WithVolume(justdirectory_portainer_volume, "/data");
 
 var postgresqlContainer = builder.AddDockerfile(
         "bwsdb",
         "../../docker",
-        "Dockerfile-dockeradmin"
+        "Dockerfile-bwsdb"
     )
     .WithContainerName("bwsdb")
     .WithEnvironment("POSTGRES_USER", "root")
     .WithEnvironment("POSTGRES_PASSWORD", "root")
     .WithEnvironment("POSTGRES_DB", "bwsdb")
-    .WithHttpEndpoint(5432, 5432)
+    .WithEndpoint(port:5432, targetPort:5432)
     .WithVolume(justdirectory_postgresql_volume, "/var/lib/postgresql/data")
     .WithBindMount(apsolutepath_initdbd_volume, "/docker-entrypoint-initdb.d");
 
 var pgadminContainer = builder.AddDockerfile(
-        "bwsdbadmin", 
+        "bwsdbadmin",
         "../../docker",
-        "Dockerfile-dockeradmin"
+        "Dockerfile-bwsdbadmin"
     )
     .WithContainerName("bwsdbadmin")
     .WithEnvironment("PGADMIN_DEFAULT_EMAIL", "admin@ta0y.com")
     .WithEnvironment("PGADMIN_DEFAULT_PASSWORD", "admin")
-    .WithHttpEndpoint(5050, 80)
+    .WithHttpEndpoint(port:5050, targetPort:80)
     .WithVolume(justdirectory_pgadmin_volume, "/var/lib/pgadmin");
 
 var rabbitMQContainer = builder.AddDockerfile(
@@ -58,8 +55,8 @@ var rabbitMQContainer = builder.AddDockerfile(
         "Dockerfile-bwsmq"
     )
     .WithContainerName("bwsmq")
-    .WithHttpEndpoint(5672, 5672)
-    .WithHttpEndpoint(15672, 15672)
+    .WithEndpoint(port:5672, targetPort:5672)
+    .WithHttpEndpoint(port:15672,targetPort:15672)
     .WithVolume("rabbitmq_volume", "/var/lib/rabbitmq");
 
 
@@ -85,7 +82,9 @@ var uvicornApp = builder.AddUvicornApp(
     .WithHttpEndpoint(
         targetPort: portValueInt,      // tatgetPort : Port the resource is listening on
         port: portValueInt + 1         // port : Port that will be exposed to the outside
-    );
+    )
+    .WaitFor(rabbitMQContainer)
+    .WaitFor(postgresqlContainer);
 #pragma warning restore ASPIREHOSTINGPYTHON001
 
 
