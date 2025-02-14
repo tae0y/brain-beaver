9a48cf642139b70d5d418966980e71e6cfa196c7
tae0y <tae0yp@gmail.com>
2025-02-15 00:00:29 +0900

    update : apphost python 설정 변경

--- src/Aspire.AppHost/Program.cs
+++ src/Aspire.AppHost/Program.cs
@@ -70,21 +70,33 @@ var portValueInt = int.TryParse(portValueStr, out int portParsed) ? portParsed :
 
 // Python Hosting은 실험적 기능으로, 추가 방법이 바뀔 수 있어 Warning을 띄움. 테스트 용도로만 사용이 권장됨.
 #pragma warning disable ASPIREHOSTINGPYTHON001
-var uvicornApp = builder.AddUvicornApp(
-        name: "python",                        // Name of the Python project
-        projectDirectory: "../Python.FastAPI", // Path to the Python project
-        appName: "app:app",                    // {FILE_NAME}:{APP_VARIABLE_NAME}
-        args: new[] { 
-            "--reload"
-        }
+var pythonApp = builder.AddPythonApp(
+        name: "python",
+        projectDirectory: "../Python.FastAPI",
+        scriptPath: "app.py",
+        scriptArgs: new[] {""}
     )
-    // 무조건 환경변수로 지정된 PORT를 따라가는 이슈가 있어 export PORT=8000 같이 지정해줌
     .WithHttpEndpoint(
-        targetPort: portValueInt,      // tatgetPort : Port the resource is listening on
-        port: portValueInt + 1         // port : Port that will be exposed to the outside
-    )
+        targetPort:portValueInt,
+        port:portValueInt+1)
     .WaitFor(rabbitMQContainer)
     .WaitFor(postgresqlContainer);
+
+//var uvicornApp = builder.AddUvicornApp(
+//        name: "python",                        // Name of the Python project
+//        projectDirectory: "../Python.FastAPI", // Path to the Python project
+//        appName: "app:app",                    // {FILE_NAME}:{APP_VARIABLE_NAME}
+//        args: new[] { 
+//            "--reload"
+//        }
+//    )
+//    // 무조건 환경변수로 지정된 PORT를 따라가는 이슈가 있어 export PORT=8000 같이 지정해줌
+//    .WithHttpEndpoint(
+//        targetPort: portValueInt,      // tatgetPort : Port the resource is listening on
+//        port: portValueInt             // port : Port that will be exposed to the outside
+//    )
+//    .WaitFor(rabbitMQContainer)
+//    .WaitFor(postgresqlContainer);
 #pragma warning restore ASPIREHOSTINGPYTHON001
 
 
