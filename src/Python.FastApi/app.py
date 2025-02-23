9c641681ac2475fdf13844254094d6872cafd25c
tae0y <tae0yp@gmail.com>
2025-02-23 16:54:44 +0900

    update : fastapi opentelemetry 구성 시도

--- src/Python.FastApi/app.py
+++ src/Python.FastApi/app.py
@@ -2,13 +2,25 @@ import os
 import logging
 import uvicorn
 from fastapi import FastAPI
+
 from starlette.middleware.cors import CORSMiddleware
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from concepts.conceptshandler import router as concepts_router
 from networks.networkshandler import router as networks_router
 from references.referenceshandler import router as references_router
 from extract.extracthandler import router as extract_router
 
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+
+# ************************************************************
+# App 정의
+# 
+# ************************************************************
 tags_metadata = [
     {"name":"Extract", "description":"데이터 추출과 관련된 요청을 처리한다."},
     {"name":"Networks", "description":"네트워크 관련 CRUD 요청을 처리한다."},
@@ -20,6 +32,11 @@ app = FastAPI(
     #root_path="/swagger"
 )
 
+
+# ************************************************************
+# App 미들웨어, 라우터 등록
+# 
+# ************************************************************
 origins = [
     "http://127.0.0.1:8111"
 ]
@@ -36,9 +53,35 @@ app.include_router(networks_router)
 app.include_router(references_router)
 app.include_router(extract_router)
 
+
+# ************************************************************
+# App 로깅, 모니터링 설정
+# 
+# - OpenTelemetry 활용방안
+#   - [ ] Grafana + Prometheus + Loki + Tempo
+#   - [ ] OpenTelemetry Collector + Something Behind
+#   - [ ] Something Ahead + Aspire Dashboard
+# ************************************************************
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+resource = Resource(attributes={"service.name": "Python.FastAPI"})
+
+# set the tracer provider
+tracer = TracerProvider(resource=resource)
+trace.set_tracer_provider(tracer)
+
+# Use the OTLPSpanExporter to send traces to Tempo
+tracer.add_span_processor(BatchSpanProcessor(OTLPSpanExporter(endpoint="http://tempo:4317")))
+
+LoggingInstrumentor().instrument()
+FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
+
+
+# ************************************************************
+# Home Page
+# 
+# ************************************************************
 @app.get("/")
 def rootPage() -> str:
     logger.info("HOME PAGE ACCESS")
