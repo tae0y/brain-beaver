b7df41de50172e481216d9c7a7346592cea3da17
tae0y <tae0yp@gmail.com>
2025-03-02 01:50:23 +0900

    update : 그래프뷰 옵션 변경

--- src/Node.ViteUI/src/data-gen.ts
+++ src/Node.ViteUI/src/data-gen.ts
@@ -1,31 +1,94 @@
 import loremIpsumKR from "lorem-ipsum-kr";
+import axios from 'axios';
 
 interface Node {
-  id: number;
+  id: string;
   label: string;
   text: string;
   value: number;
   category: string;
 }
 
+interface Link {
+  source: number;
+  target: number;
+}
+
+const readyCallbacks: ((data: { pointPositions: Node[], links: number[] }) => void)[] = [];
+
+function notifyReady() {
+  readyCallbacks.forEach(callback => callback({ pointPositions, links }));
+}
+
+export function onDataReady(callback: (data: { pointPositions: Node[], links: number[] }) => void) {
+  readyCallbacks.push(callback);
+}
+
 function getRandom(min: number, max: number): number {
-    return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+async function fetchConcepts() {
+  try {
+    //const response = await axios.get('http://bws_backend:8112/api/concepts');
+    const concepts_response = await axios.get('http://localhost:8112/api/concepts');
+    if (concepts_response.data.status === 'success') {
+      const concepts = concepts_response.data.data;
+      return concepts.map((concept: any) => ({
+        id: concept.id,
+        label: concept.keywords,
+        text: concept.summary,
+        value: concept.source_num + concept.target_num,
+        category: concept.category
+      }));
+    } else {
+      console.error('Failed to fetch concepts:', concepts_response.data.message);
+      return [];
+    }
+  } catch (error) {
+    console.error('Error fetching concepts:', error);
+    return [];
+  }
+}
+
+async function fetchNetworks(){
+  try {
+    //const response = await axios.get('http://bws_backend:8112/api/concepts');
+    const networks_response = await axios.get('http://localhost:8112/api/networks');
+    if (networks_response.data.status === 'success') {
+      const networks = networks_response.data.data;
+      const results = networks.map((network: any) => ({
+        source: network.source_concept_id,
+        target: network.target_concept_id
+      }));
+      return results
+    } else {
+      console.error('Failed to fetch concepts:', networks_response.data.message);
+      return [];
+    }
+  } catch (error) {
+    console.error('Error fetching concepts:', error);
+    return [];
+  }
+}
+
+let pointPositions: Node[] = [];
+let links: Link[] = []; //source, target 순서로 flat하게
+
+(async () => {
+  pointPositions = await fetchConcepts();
+  links = await fetchNetworks();
+  if (pointPositions.length % 2 != 0) {
+    pointPositions.push({
+      id: -1,
+      label: "dummy",
+      text: "dummy",
+      value: 0,
+      category: "dummy"
+    });
   }
 
-const numNodes = 20000;
-const pointPositions: Node[] = Array.from({ length: numNodes }, (_, i) => ({
-  id: i + 1,
-  label: `노드 ${i + 1}`,
-  text: loremIpsumKR(),
-  value: Math.floor(Math.random() * 100) + 1,
-  category: String.fromCharCode(65 + Math.floor(Math.random() * 26))
-}));
-
-const numLinks = Math.floor(numNodes * 2);
-const links: number[] = [];
-Array.from({ length: numLinks }, () => (
-  links.push(Math.floor(Math.random() * numNodes) + 1),
-  links.push(Math.floor(Math.random() * numNodes) + 1)
-));
+  notifyReady();
+})();
 
 export { pointPositions, links };
--- src/Node.ViteUI/src/main.ts
+++ src/Node.ViteUI/src/main.ts
@@ -1,120 +1,115 @@
 // refer to https://cosmograph.app/docs/cosmograph/Introduction
 
 import './style.css';
-import { pointPositions, links } from './data-gen';
+import { pointPositions, links, onDataReady } from './data-gen';
+import { Cosmograph } from '@cosmograph/cosmograph'
 import { Graph, GraphConfigInterface } from '@cosmograph/cosmos';
 
-const div = document.getElementById('graph') as HTMLDivElement;
-let graph: Graph;
-const config: GraphConfigInterface = {
-  spaceSize: 4096,
-  backgroundColor: '#151515',
-  pointSize: 4,
-  pointColor: '#4B5BBF',
-  pointGreyoutOpacity: 0.1,
-  linkWidth: 0.1,
-  linkColor: '#5F74C2',
-  linkArrows: false,
-  linkGreyoutOpacity: 0,
-  curvedLinks: true,
-  renderHoveredPointRing: true,
-  hoveredPointRingColor: '#4B5BBF',
-  enableDrag: true,
-  simulationLinkDistance: 1,
-  simulationLinkSpring: 2,
-  simulationRepulsion: 0.2,
-  simulationGravity: 0.1,
-  simulationDecay: 100000,
-  onClick: (
-    index: number | undefined,
-    pointPosition: [number, number] | undefined,
-    event: MouseEvent
-  ) => {
-    if (index !== undefined) {
-      graph.selectPointByIndex(index);
-      graph.zoomToPointByIndex(index);
-    } else {
-      graph.unselectPoints();
-    }
-    console.log('Clicked point index: ', index);
-  },
-};
+onDataReady(initGraph);
 
-graph = new Graph(div, config);
-graph.setPointPositions(pointPositions);
-graph.setLinks(links);
+const colors = ['#88C6FF', '#FF99D2', '#2748A4'];
 
-graph.zoom(0.9);
-graph.render();
+function initGraph(){
+  const div = document.getElementById('graph') as HTMLDivElement;
+  document.body.appendChild(div);
+  const graph = new Cosmograph(div)
 
-/* ~ Demo Actions ~ */
-// Start / Pause
-let isPaused = false;
-const pauseButton = document.getElementById('pause') as HTMLDivElement;
+  const config = {
+    renderHoveredPointRing: true,
+    hoveredNodeRingColor:'red',
+    focusedNodeRingColor: 'yellow', 
+    showDynamicLabels: true,
+    nodeSize: 1,
+    nodeColor: () => colors[Math.floor(Math.random() * colors.length)],
+    linkWidth: () => 1 + 2 * Math.random(),
+    linkColor: () => colors[Math.floor(Math.random() * colors.length)],
+    spaceSize: 4096,
+    backgroundColor: '#151515',
+    pointGreyoutOpacity: 0.1,
+    linkArrows: false,
+    linkGreyoutOpacity: 0,
+    curvedLinks: true,
+    enableDrag: true,
+    simulationFriction: 0.1,
+    simulationLinkSpring: 0.5, 
+    simulationLinkDistance: 2.0,
+    simulationRepulsion: 0.2,
+    simulationGravity: 0.1,
+    simulationDecay: 100000
+  }
 
-function pause() {
-  isPaused = true;
-  pauseButton.textContent = 'Start';
-  graph.pause();
+  graph.setConfig(config)
+  graph.setData(pointPositions, links)
+  
+  /* ~ Demo Actions ~ */
+  // Start / Pause
+  let isPaused = false;
+  const pauseButton = document.getElementById('pause') as HTMLDivElement;
+  
+  function pause() {
+    isPaused = true;
+    pauseButton.textContent = 'Start';
+    graph.pause();
+  }
+  
+  function start() {
+    isPaused = false;
+    pauseButton.textContent = 'Pause';
+    graph.start();
+  }
+  
+  function togglePause() {
+    if (isPaused) start();
+    else pause();
+  }
+  
+  pauseButton.addEventListener('click', togglePause);
+  
+  // Zoom and Select
+  function getRandomPointIndex() {
+    return Math.floor((Math.random() * pointPositions.length) / 2);
+  }
+  
+  function getRandomInRange([min, max]: [number, number]): number {
+    return Math.random() * (max - min) + min;
+  }
+  
+  function fitView() {
+    graph.fitView();
+  }
+  
+  function zoomIn() {
+    const pointIndex = getRandomPointIndex();
+    graph.zoomToPointByIndex(pointIndex);
+    graph.selectPointByIndex(pointIndex);
+    pause();
+  }
+  
+  function selectPoint() {
+    const pointIndex = getRandomPointIndex();
+    graph.selectPointByIndex(pointIndex);
+    graph.fitView();
+    pause();
+  }
+  
+  function selectPointsInArea() {
+    const w = canvas.clientWidth;
+    const h = canvas.clientHeight;
+    const left = getRandomInRange([w / 4, w / 2]);
+    const right = getRandomInRange([left, (w * 3) / 4]);
+    const top = getRandomInRange([h / 4, h / 2]);
+    const bottom = getRandomInRange([top, (h * 3) / 4]);
+    pause();
+    graph.selectPointsInRange([
+      [left, top],
+      [right, bottom],
+    ]);
+  }
+  
+  document.getElementById('fit-view')?.addEventListener('click', fitView);
+  document.getElementById('zoom')?.addEventListener('click', zoomIn);
+  document.getElementById('select-point')?.addEventListener('click', selectPoint);
+  document
+    .getElementById('select-points-in-area')
+    ?.addEventListener('click', selectPointsInArea);
 }
-
-function start() {
-  isPaused = false;
-  pauseButton.textContent = 'Pause';
-  graph.start();
-}
-
-function togglePause() {
-  if (isPaused) start();
-  else pause();
-}
-
-pauseButton.addEventListener('click', togglePause);
-
-// Zoom and Select
-function getRandomPointIndex() {
-  return Math.floor((Math.random() * pointPositions.length) / 2);
-}
-
-function getRandomInRange([min, max]: [number, number]): number {
-  return Math.random() * (max - min) + min;
-}
-
-function fitView() {
-  graph.fitView();
-}
-
-function zoomIn() {
-  const pointIndex = getRandomPointIndex();
-  graph.zoomToPointByIndex(pointIndex);
-  graph.selectPointByIndex(pointIndex);
-  pause();
-}
-
-function selectPoint() {
-  const pointIndex = getRandomPointIndex();
-  graph.selectPointByIndex(pointIndex);
-  graph.fitView();
-  pause();
-}
-
-function selectPointsInArea() {
-  const w = canvas.clientWidth;
-  const h = canvas.clientHeight;
-  const left = getRandomInRange([w / 4, w / 2]);
-  const right = getRandomInRange([left, (w * 3) / 4]);
-  const top = getRandomInRange([h / 4, h / 2]);
-  const bottom = getRandomInRange([top, (h * 3) / 4]);
-  pause();
-  graph.selectPointsInRange([
-    [left, top],
-    [right, bottom],
-  ]);
-}
-
-document.getElementById('fit-view')?.addEventListener('click', fitView);
-document.getElementById('zoom')?.addEventListener('click', zoomIn);
-document.getElementById('select-point')?.addEventListener('click', selectPoint);
-document
-  .getElementById('select-points-in-area')
-  ?.addEventListener('click', selectPointsInArea);
