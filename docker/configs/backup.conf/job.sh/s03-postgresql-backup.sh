9b380a20fcc2d089c34134360d63b583173119a5
tae0y <tae0yp@gmail.com>
2025-03-08 12:46:00 +0900

    도커 볼륨 백업 롤링주기 조정(7일->2일)

--- docker/configs/backup.conf/job.sh/s03-postgresql-backup.sh
+++ docker/configs/backup.conf/job.sh/s03-postgresql-backup.sh
@@ -9,4 +9,4 @@ backup_file="${target_dir}/${current_date}-postgresql.cpio.gz"
 mkdir -p "$target_dir"
 
 find "$source_dir" -print | cpio -o -H newc | gzip > "$backup_file"
-find "$target_dir" -name "*.cpio.gz" -type f -mtime +7 -exec rm {} \;
+find "$target_dir" -name "*.cpio.gz" -type f -mtime +2 -exec rm {} \;
